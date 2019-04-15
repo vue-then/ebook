bcae07eb9151aab7717650b9704d03b3983b30e9
js103 <643929860@qq.com>
2019-04-15 16:42:01 +0700

    touchstart

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -1,26 +1,25 @@
 <style lang="scss" rel="stylesheet/scss" scoped>
-  @import "../../assets/styles/global";
+	@import "../../assets/styles/global";
 
-  .ebook-reader {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    .ebook-reader-mask {
-      position: absolute;
-      z-index: 150;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-  }
+	.ebook-reader {
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		.ebook-reader-mask {
+			position: absolute;
+			z-index: 150;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
 
 <template>
 	<div class="ebook-reader">
-    <div id="read"></div>
-    
-  </div>
+		<div id="read"></div>
+	</div>
 </template>
 <script>
 import Epub from "epubjs";
@@ -46,27 +45,59 @@ export default {
 			const baseUrl = "/resource/epub/";
 			const url = baseUrl + this.fileName + ".epub";
 			this.book = new Epub(url);
-            // console.log( this.book,bookHome,url);
-            // http://localhost:8092/#/ebook/History|2017_Book_InterdisciplinaryPerspectivesO at
-			this.rendition = this.book.renderTo('read',{
+			// console.log( this.book,bookHome,url);
+			// http://localhost:8092/#/ebook/History|2017_Book_InterdisciplinaryPerspectivesO at
+			this.rendition = this.book.renderTo("read", {
 				width: window.innerWidth,
-				height: window.innerHeight,
-				method: "default"
-            });
-            this.rendition.display()
+				height: window.innerHeight
+				// method: "default"
+			});
+			this.rendition.display();
 			// axios.get('/book/home').then(res =>{
 			//     console.log(res,'123456')
-            // })
-             this.rendition.on('touchstart', event => {
-                console.log(event,'touchstart')
-                this.touchStartX = event.changedTouches[0].clientX
-                this.touchStartTime = event.timeStamp
-            })
-            this.rendition.on('touchend',event => {
-                console.log(event,12345)
-                event.preventDefault()
-                event.stopPropagation()
-            })
+			// })
+			this.rendition.on("touchstart", event => {
+				this.touchStartX = event.changedTouches[0].clientX;
+				this.touchStartTime = event.timeStamp;
+			});
+			this.rendition.on("touchend", event => {
+				const offsetX =
+					event.changedTouches[0].clientX - this.touchStartX;
+				const time = event.timeStamp - this.touchStartTime;
+
+				if (time < 500 && offsetX > 40) {
+					//touch_左
+					this.prevPage();
+					console.log(1);
+				} else if (time < 500 && offsetX < -40) {
+					//touch_右
+					this.nextPage();
+					console.log(12);
+				} else {
+					//touch_中间
+					// this.toggleTitleAndMenu();
+					console.log(123);
+				}
+
+				event.preventDefault();
+				event.stopPropagation();
+			});
+		},
+		prevPage() {
+			if (this.rendition) {
+				this.rendition.prev().then(() => {
+					// this.refreshLocation();
+				});
+				// this.hideTitleAndMenu();
+			}
+		},
+		nextPage() {
+			if (this.rendition) {
+				this.rendition.next().then(() => {
+					// this.refreshLocation();
+				});
+				// this.hideTitleAndMenu();
+			}
 		}
 	}
 };
